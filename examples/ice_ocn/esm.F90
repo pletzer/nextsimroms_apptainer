fc36894cd6c44147977838a4ae5bdbc8954b25c4
Alexander Pletzer <alexander@gokliya.net>
2024-10-25 11:27:44 +1300

    working on examples

---
+++ examples/ice_ocn/esm.F90
@@ -0,0 +1,230 @@
+!==============================================================================
+! Earth System Modeling Framework
+! Copyright (c) 2002-2024, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the University of Illinois-NCSA License.
+!==============================================================================
+
+module ESM
+
+  !-----------------------------------------------------------------------------
+  ! Code that specializes generic ESM Component code.
+  !-----------------------------------------------------------------------------
+
+  ! Enabling the followng macro, i.e. setting it to WITHPETLISTS_on, will
+  ! activate sections of code that demonstrate how
+  ! the ICE and OCN components can run on exclusive sets of PETs. Turning this
+  ! on/off does not affect how the Connector component is specialized.
+#define WITHPETLISTS_on
+
+  use ESMF
+  use NUOPC
+  use NUOPC_Driver, &
+    driverSS             => SetServices
+
+  use ICE, only: iceSS => SetServices
+  use OCN, only: ocnSS => SetServices
+
+  use CON, only: cplSS => SetServices
+
+  implicit none
+
+  private
+
+  public SetServices
+
+  !-----------------------------------------------------------------------------
+  contains
+  !-----------------------------------------------------------------------------
+
+  subroutine SetServices(driver, rc)
+    type(ESMF_GridComp)  :: driver
+    integer, intent(out) :: rc
+
+    rc = ESMF_SUCCESS
+
+    ! derive from NUOPC_Driver
+    call NUOPC_CompDerive(driver, driverSS, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    ! specialize driver
+    call NUOPC_CompSpecialize(driver, specLabel=label_SetModelServices, &
+      specRoutine=SetModelServices, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    ! set driver verbosity
+    call NUOPC_CompAttributeSet(driver, name="Verbosity", value="high", rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+  end subroutine
+
+  !-----------------------------------------------------------------------------
+
+  subroutine SetModelServices(driver, rc)
+    type(ESMF_GridComp)  :: driver
+    integer, intent(out) :: rc
+
+    ! local variables
+    type(ESMF_Grid)               :: grid
+    type(ESMF_Field)              :: field
+    type(ESMF_Time)               :: startTime
+    type(ESMF_Time)               :: stopTime
+    type(ESMF_TimeInterval)       :: timeStep
+    type(ESMF_Clock)              :: internalClock
+    type(ESMF_GridComp)           :: child
+    type(ESMF_CplComp)            :: conn
+    integer                       :: petCount, i
+    integer, allocatable          :: petList(:)
+    integer                       :: verbosity
+    character(len=10)             :: attrStr
+
+    rc = ESMF_SUCCESS
+
+   ! get the petCount
+    call ESMF_GridCompGet(driver, petCount=petCount, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    ! SetServices for ICE with petList on first half of PETs
+#ifdef WITHPETLISTS_on
+    allocate(petList(petCount/2))
+    do i=1, petCount/2
+      petList(i) = i-1 ! PET labeling goes from 0 to petCount-1
+    enddo
+#endif
+    call NUOPC_DriverAddComp(driver, "ICE", iceSS, &
+#ifdef WITHPETLISTS_on
+      petList=petList, &
+#endif
+      comp=child, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+#ifdef WITHPETLISTS_on
+    deallocate(petList)
+#endif
+    verbosity = 0 ! reset
+    verbosity = ibset(verbosity,0)  ! log basic intro/extro and indentation
+    write(attrStr,"(I10)") verbosity
+    call NUOPC_CompAttributeSet(child, name="Verbosity", value=attrStr, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    ! SetServices for OCN with petList on second half of PETs
+#ifdef WITHPETLISTS_on
+    allocate(petList(petCount/2))
+    do i=1, petCount/2
+      petList(i) = petCount/2 + i-1 ! PET labeling goes from 0 to petCount-1
+    enddo
+#endif
+    call NUOPC_DriverAddComp(driver, "OCN", ocnSS, &
+#ifdef WITHPETLISTS_on
+      petList=petList, &
+#endif
+      comp=child, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+#ifdef WITHPETLISTS_on
+    deallocate(petList)
+#endif
+    verbosity = 0 ! reset
+    verbosity = ibset(verbosity,0)  ! log basic intro/extro and indentation
+    write(attrStr,"(I10)") verbosity
+    call NUOPC_CompAttributeSet(child, name="Verbosity", value=attrStr, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    ! SetServices for ice2ocn
+    call NUOPC_DriverAddComp(driver, srcCompLabel="ICE", dstCompLabel="OCN", &
+      compSetServicesRoutine=cplSS, comp=conn, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    verbosity = 0 ! reset
+    verbosity = ibset(verbosity,0)  ! log basic intro/extro and indentation
+    verbosity = ibset(verbosity,12) ! log CplList loop
+    write(attrStr,"(I10)") verbosity
+    call NUOPC_CompAttributeSet(conn, name="Verbosity", value=attrStr, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    ! SetServices for ocn2ice
+    call NUOPC_DriverAddComp(driver, srcCompLabel="OCN", dstCompLabel="ICE", &
+      compSetServicesRoutine=cplSS, comp=conn, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    verbosity = 0 ! reset
+    verbosity = ibset(verbosity,0)  ! log basic intro/extro and indentation
+    verbosity = ibset(verbosity,12) ! log CplList loop
+    write(attrStr,"(I10)") verbosity
+    call NUOPC_CompAttributeSet(conn, name="Verbosity", value=attrStr, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    ! set the driver clock
+    call ESMF_TimeIntervalSet(timeStep, m=15, rc=rc) ! 15 minute steps
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    call ESMF_TimeSet(startTime, yy=2010, mm=6, dd=1, h=0, m=0, &
+      calkindflag=ESMF_CALKIND_GREGORIAN, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    call ESMF_TimeSet(stopTime, yy=2010, mm=6, dd=1, h=1, m=0, &
+      calkindflag=ESMF_CALKIND_GREGORIAN, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    internalClock = ESMF_ClockCreate(name="Application Clock", &
+      timeStep=timeStep, startTime=startTime, stopTime=stopTime, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    call ESMF_GridCompSet(driver, clock=internalClock, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+  end subroutine
+
+  !-----------------------------------------------------------------------------
+
+end module
