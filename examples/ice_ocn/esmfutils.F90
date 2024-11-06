2c8b2388a799b11ff41611df505c105eda62ea23
Alexander Pletzer <alexander@gokliya.net>
2024-11-07 11:34:03 +1300

    refactoring

--- examples/ice_ocn/esmfutils.F90
+++ examples/ice_ocn/esmfutils.F90
@@ -6,25 +6,20 @@
 
 contains
 
-    subroutine esmfutils_getImportDataPtr(model, name, ptr, rc)
+    subroutine esmfutils_getDataPtrFromState(state, name, ptr, rc)
 
         implicit none
-        type(ESMF_GridComp)  :: model
+        type(ESMF_State), intent(in) :: state
         character(len=*), intent(in) :: name
         real(8), pointer, intent(out) :: ptr(:, :)
         integer, intent(out) :: rc
 
-        type(ESMF_State)        :: state
         type(ESMF_Field)        :: field
         type(ESMF_Array)        :: array
         integer :: rc2
     
         rc = ESMF_SUCCESS
     
-        ! query for importState and exportState
-        call NUOPC_ModelGet(model,  importState=state, rc=rc2)
-        if (rc2 /= ESMF_SUCCESS) rc = rc + 1
-
         ! retrieve the field
         call ESMF_StateGet(state, itemName=name, field=field, rc=rc2)
         if (rc2 /= ESMF_SUCCESS) rc = rc + 1
@@ -35,6 +30,28 @@ contains
   
         ! get local pointer to the data. We only have one domain per PE
         call ESMF_ArrayGet(array, localDe=0, farrayPtr=ptr, rc=rc2)
+        if (rc2 /= ESMF_SUCCESS) rc = rc + 1
+
+    end subroutine esmfutils_getDataPtrFromState
+
+    subroutine esmfutils_getImportDataPtr(model, name, ptr, rc)
+
+        implicit none
+        type(ESMF_GridComp)  :: model
+        character(len=*), intent(in) :: name
+        real(8), pointer, intent(out) :: ptr(:, :)
+        integer, intent(out) :: rc
+
+        type(ESMF_State)        :: state
+        integer :: rc2
+    
+        rc = ESMF_SUCCESS
+    
+        ! query for importState and exportState
+        call NUOPC_ModelGet(model,  importState=state, rc=rc2)
+        if (rc2 /= ESMF_SUCCESS) rc = rc + 1
+
+        call esmfutils_getDataPtrFromState(state, name, ptr, rc2)
         if (rc2 /= ESMF_SUCCESS) rc = rc + 1
     
     end subroutine esmfutils_getImportDataPtr
