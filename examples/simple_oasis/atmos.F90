951347953e4b18c7ed5257d08eab814220138b55
Alexander Pletzer <alexander@gokliya.net>
2025-01-15 09:12:37 +1300

    runs

--- examples/simple_oasis/atmos.F90
+++ examples/simple_oasis/atmos.F90
@@ -5,7 +5,9 @@ PROGRAM ocean
 
   IMPLICIT NONE
 
-  integer :: ier, local_comm, npes, mype
+  integer :: ier, local_comm, coupl_comm, npes, mype
+  integer :: comp_id, partition_id, paral(3)
+  integer :: nlon = 2, nlat = 3
 
   call MPI_Init(ier)
   local_comm = MPI_COMM_WORLD
@@ -13,9 +15,21 @@ PROGRAM ocean
   CALL MPI_Comm_Rank (local_comm, mype, ier)
   print *,'atmos [', mype, '] out of ', npes
   !
+  ! coupled = .FALSE. to start with
+  call oasis_init_comp(comp_id, 'ocean', ier, .FALSE., local_comm)
 
+  call oasis_get_localcomm(local_comm, ier)
 
+  ! call oasis_create_couplcomm(1, local_comm, coupl_comm, ier)
 
+  paral(1) = 1
+  paral(2) = 0
+  paral(3) = nlon * nlat
+  call oasis_def_partition(partition_id, paral, ier)
+
+  call oasis_enddef(ier)
+
+  call oasis_terminate(ier)
 
   CALL MPI_Finalize(ier)
   !
--- examples/simple_oasis/ocean.F90
+++ examples/simple_oasis/ocean.F90
@@ -4,16 +4,31 @@ PROGRAM ocean
   use mod_oasis
   
 
-  integer :: ier, local_comm, npes, mype
-  integer :: comp_id
-  integer :: nlon, nlat, il_extentx, il_extenty, il_size, il_offsetx, il_offsety, il_offset
+  integer :: ier, local_comm, coupl_comm, npes, mype
+  integer :: comp_id, partition_id, paral(3)
+  integer :: nlon = 3, nlat = 2
 
   call MPI_Init(ier)
   local_comm =  MPI_COMM_WORLD
   CALL MPI_Comm_Size (local_comm, npes, ier)
   CALL MPI_Comm_Rank (local_comm, mype, ier)
   print *,'ocean [', mype, '] out of ', npes
-  !
+
+  ! coupled = .FALSE. to start with
+  call oasis_init_comp(comp_id, 'ocean', ier, .FALSE., local_comm)
+
+  call oasis_get_localcomm(local_comm, ier)
+
+  ! call oasis_create_couplcomm(1, local_comm, coupl_comm, ier)
+
+  paral(1) = 1
+  paral(2) = 0
+  paral(3) = nlon * nlat
+  call oasis_def_partition(partition_id, paral, ier)
+
+  call oasis_enddef(ier)
+
+  call oasis_terminate(ier)
   !
   CALL MPI_Finalize(ier)
   !
