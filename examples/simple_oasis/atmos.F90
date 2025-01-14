312b7dcf033fedea347cbe96bf52954730e0312f
Alexander Pletzer <alexander@gokliya.net>
2025-01-15 08:43:35 +1300

    starting fresh

--- examples/simple_oasis/atmos.F90
+++ examples/simple_oasis/atmos.F90
@@ -1,34 +1,23 @@
 PROGRAM ocean
   !
-  ! Use for netCDF library
-  USE netcdf
   use mpi
+  use mod_oasis
+
   IMPLICIT NONE
 
-  integer :: ierror, local_comm, npes, mype
+  integer :: ier, local_comm, npes, mype
 
-  call MPI_Init(ierror)
+  call MPI_Init(ier)
+  local_comm = MPI_COMM_WORLD
+  CALL MPI_Comm_Size (local_comm, npes, ier)
+  CALL MPI_Comm_Rank (local_comm, mype, ier)
+  print *,'atmos [', mype, '] out of ', npes
   !
-  local_comm =  MPI_COMM_WORLD
-  !
-  !!!!!!!!!!!!!!!!! OASIS_INIT_COMP !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  !
-  !!!!!!!!!!!!!!!!! OASIS_GET_LOCALCOMM !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  !
-  ! Get rank in local communicator
-  CALL MPI_Comm_Size ( local_comm, npes, ierror )
-  CALL MPI_Comm_Rank ( local_comm, mype, ierror )
 
-  print *,'atmos [', mype, ']'
-  !
-  !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  !         TERMINATION 
-  !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  !
-  !!!!!!!!!!!!!!!!!! OASIS_TERMINATE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  !
-  !
-  CALL MPI_Finalize(ierror)
+
+
+
+  CALL MPI_Finalize(ier)
   !
 END PROGRAM ocean
 !
--- examples/simple_oasis/ocean.F90
+++ examples/simple_oasis/ocean.F90
@@ -1,47 +1,21 @@
 PROGRAM ocean
   !
-  ! Use for netCDF library
-  USE netcdf
   use mpi
   use mod_oasis
-  use def_parallel_decomposition
-  IMPLICIT NONE
+  
 
-  integer :: ierror, local_comm, npes, mype
+  integer :: ier, local_comm, npes, mype
   integer :: comp_id
   integer :: nlon, nlat, il_extentx, il_extenty, il_size, il_offsetx, il_offsety, il_offset
 
-  call MPI_Init(ierror)
-  !
+  call MPI_Init(ier)
   local_comm =  MPI_COMM_WORLD
-  !
-  !!!!!!!!!!!!!!!!! OASIS_INIT_COMP !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  !
-  !!!!!!!!!!!!!!!!! OASIS_GET_LOCALCOMM !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  !
-  ! Get rank in local communicator
-  CALL MPI_Comm_Size ( local_comm, npes, ierror )
-  CALL MPI_Comm_Rank ( local_comm, mype, ierror )
-
-  call oasis_init_comp(comp_id, 'ocn', ierror)
-
-  nlon = 4
-  nlat = 2
-  call def_local_partition(nlon, nlat, npes, mype, &
-    & il_extentx, il_extenty, il_size, il_offsetx, il_offsety, il_offset)
-  print *,'ocean [', mype, '] il_extentx, il_extenty, il_size, il_offsetx, il_offsety, il_offset = ', &
-                           &  il_extentx, il_extenty, il_size, il_offsetx, il_offsety, il_offset
-
-  !
-  !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  !         TERMINATION 
-  !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  !
-  !!!!!!!!!!!!!!!!!! OASIS_TERMINATE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  call oasis_terminate()
+  CALL MPI_Comm_Size (local_comm, npes, ier)
+  CALL MPI_Comm_Rank (local_comm, mype, ier)
+  print *,'ocean [', mype, '] out of ', npes
   !
   !
-  CALL MPI_Finalize(ierror)
+  CALL MPI_Finalize(ier)
   !
 END PROGRAM ocean
 !
