6cc24058e448c8a34dfee791b2c5c774b32fee24
Alexander Pletzer <alexander@gokliya.net>
2025-03-18 14:46:27 +1300

    now compiles

---
+++ examples/test_exchange/diffusion/generic_component_mod.F90
@@ -0,0 +1,139 @@
+module generic_component_mod
+
+    use mod_oasis
+    implicit none
+    integer, parameter :: STR_LEN = 16
+    
+    type generic_component_type
+        real(8), allocatable :: temperature(:, :, :)
+        real(8), allocatable :: new_temperature(:, :, :)
+        real(8), allocatable :: top_temperature(:, :)
+        real(8), allocatable :: bottom_temperature(:, :)
+        real(8) :: dx
+        real(8) :: dt
+        real(8) :: kappa
+        integer :: num_steps
+    end type generic_component_type
+
+    contains
+
+    subroutine gc_new(self, namelist_file, ier)
+
+        implicit none
+
+        type(generic_component_type), intent(inout) :: self
+        character(len=*), intent(in) :: namelist_file
+        integer, intent(out) :: ier
+
+        integer :: nx, ny, nz, num_steps, iu
+        real(8) :: kappa, dx, dt
+        
+        namelist /model/ nx, ny, nz, num_steps, kappa, dt, dx
+
+        dx = 0
+        dt = 0
+        kappa = 0.
+        num_steps = 0
+        nx = 1
+        ny = 1
+        nz = 1
+        open(newunit=iu, file=namelist_file, status='old', iostat=ier); if (ier /= 0) return
+        read(unit=iu, nml=model, iostat=ier); if (ier /= 0) return        
+        close(iu)
+    
+        self % dx = dx
+        self % dt = dt
+        self % kappa = kappa
+        self % num_steps = num_steps
+        allocate(self % temperature(nx, ny, nz))
+        allocate(self % new_temperature(nx, ny, nz))
+        allocate(self % bottom_temperature(nx, ny))  
+        allocate(self % top_temperature(nx, ny))  
+            
+    end subroutine gc_new
+
+    subroutine gc_step(self, ier)
+        implicit none
+        type(generic_component_type), intent(inout) :: self
+        integer, intent(out) :: ier
+
+        integer :: i, j, k, ip, jp, kp, im, jm, km, nx, ny, nz
+        real(8) :: coeff
+
+        ier = 0
+        
+        coeff = self % kappa * self % dt / self % dx**2
+        nx = size(self % temperature, 1)
+        ny = size(self % temperature, 2)
+        nz = size(self % temperature, 3)
+    
+        ! inner domain
+        do k = 2, size(self% temperature, 3) - 1
+            km = k - 1
+            kp = k + 1
+            do j = 1, size(self % temperature, 2)
+                jm = j - 1
+                if (jm <= 0) jm = jm + 1
+                jp = j + 1
+                if (jp > ny) jp = jp - ny
+                do i = 1, size(self % temperature, 1)
+                    im = i - 1
+                    if (im <= 0) im = im + nx
+                    ip = i + 1
+                    if (ip > nx) ip = ip - nx
+                    self % new_temperature(i, j, k) = self % temperature(i, j, k) + &
+                    & coeff * ( & 
+                    &   self % temperature(ip, j, k) + self % temperature(im, j, k) +  &
+                    &   self % temperature(i, jp, k) + self % temperature(i, jm, k) +  &
+                    &   self % temperature(i, j, kp) + self % temperature(i, j, km) +  &
+                    &  - 6*self % temperature(i, j, k)                                 &
+                        )
+                enddo
+            enddo
+        enddo
+
+        ! bottom and top
+        do j = 1, size(self % temperature, 2)
+            jm = j - 1
+            if (jm <= 0) jm = jm + 1
+            jp = j + 1
+            if (jp > ny) jp = jp - ny
+            do i = 1, size(self % temperature, 1)
+                im = i - 1
+                if (im <= 0) im = im + nx
+                ip = i + 1
+                if (ip > nx) ip = ip - nx
+                ! bottom
+                self % new_temperature(i, j, 1) = self % bottom_temperature(i, j)
+                ! top
+                self % new_temperature(i, j, nz) = self % top_temperature(i, j)
+            enddo
+        enddo
+    
+        ! update, entire domain
+        do k = 1, size(self% temperature, 3)
+            do j = 1, size(self % temperature, 2)
+                do i = 1, size(self % temperature, 1)
+                    self % temperature(i, j, k) = self % new_temperature(i, j, k)
+                enddo
+            enddo
+        enddo
+                        
+           
+    end subroutine gc_step
+
+    subroutine gc_del(self, ier)
+        implicit none
+        type(generic_component_type), intent(inout) :: self
+        integer, intent(out) :: ier
+        integer :: status
+        ier = 0
+        ! do not generate an error if deallocating an array that is not allocated
+        deallocate(self % temperature, stat=status)
+        deallocate(self % new_temperature, stat=status)
+        deallocate(self % bottom_temperature, stat=status)
+        deallocate(self % top_temperature, stat=status)
+    end subroutine gc_del
+
+end module generic_component_mod
+    
---
+++ examples/test_exchange/diffusion/ocean.F90
@@ -0,0 +1,120 @@
+program ocean
+   use mpi
+   use mod_oasis
+   use generic_component_mod
+   use exception_mod
+   implicit none
+   character(len=5), parameter :: comp_name = 'ocean'
+   integer :: i, k, kinfo, date
+   integer :: comp_id, part_id, export_id, import_id
+   integer :: part_params(OASIS_Serial_Params), offset, local_size
+   integer :: local_comm, comm_size, comm_rank
+   integer :: var_nodims(2)
+
+   integer :: nx_global, ny_global
+   integer :: n_points     ! total number of points
+
+   type(generic_component_type) :: component
+   integer :: n_export, n_import
+
+   call mpi_init(kinfo)
+
+   call oasis_init_comp(comp_id, comp_name, kinfo)
+   if(kinfo<0) call oasis_abort(comp_id, comp_name, &
+      & "Error in oasis_init_comp: ", rcode=kinfo)
+
+   call oasis_get_localcomm(local_comm, kinfo)
+   if(kinfo<0) call oasis_abort(comp_id, comp_name, &
+      & "Error in oasis_get_localcomm: ", rcode=kinfo)
+
+   call mpi_comm_size(local_comm, comm_size, kinfo)
+   call mpi_comm_rank(local_comm, comm_rank, kinfo)   
+   print *, comp_name, ": Component ID: ", comp_id
+
+   call gc_new(component, 'oi_data/ocean.nml', kinfo)
+   call check_err(kinfo, comp_id, comp_name, __FILE__, __LINE__)
+
+   nx_global = size(component % temperature, 1)
+   ny_global = size(component % temperature, 2)
+   n_points = nx_global*ny_global
+
+   ! Domain decomposition
+
+   ! local_size=n_points/comm_size
+   ! offset=comm_rank*local_size
+   ! if (comm_rank == comm_size - 1) &
+   !    & local_size = n_points - offset
+
+   ! part_params(OASIS_Strategy) = OASIS_Apple
+   ! part_params(OASIS_Offset)   = offset
+   ! part_params(OASIS_Length)   = local_size
+
+   ! no decomp for the time being
+   part_params(OASIS_Strategy) = OASIS_Serial
+   part_params(OASIS_Length)   = n_points
+   
+   call oasis_def_partition(part_id, part_params, kinfo)
+   if(kinfo<0) call oasis_abort(comp_id, comp_name, &
+      & "Error in oasis_def_partition: ", rcode=kinfo)
+
+   var_nodims=[1, 1]
+   
+   call oasis_def_var(export_id, 'O_SST_FROM_OCN', &
+      &               part_id, var_nodims, OASIS_OUT, &
+      &               OASIS_DOUBLE, kinfo)
+   if(kinfo<0 .or. export_id < 0) &
+      & call oasis_abort(comp_id, comp_name, &
+      & "Error in oasis_def_var: ", rcode=kinfo)
+
+   call oasis_def_var(import_id, 'O_SST_FROM_ICE', &
+      &               part_id, var_nodims, OASIS_IN, &
+      &                 OASIS_DOUBLE, kinfo)
+   if(kinfo<0 .or. import_id < 0) &
+      & call oasis_abort(comp_id, comp_name, &
+      & "Error in oasis_def_var: ", rcode=kinfo)
+   
+   call oasis_enddef(kinfo)
+   if(kinfo<0) call oasis_abort(comp_id, comp_name, &
+      & "Error in oasis_enddef: ", rcode=kinfo)
+
+   ! initialize the temperature of this component to the bottom temperature
+   do k = 1, size(component % temperature, 3)
+      component % temperature(:, :, k) = component % bottom_temperature
+   enddo
+   
+     
+   ! data is the number of seconds into the simulation 
+   date = 0
+   do date = 0, component % num_steps
+
+      ! set the top temperature
+      component % top_temperature = component % temperature(:, :, size(component % temperature, 3))
+
+      ! export the temperature to ice
+      call oasis_put(export_id, date, component % top_temperature, kinfo)
+      if(kinfo<0) call oasis_abort(comp_id, comp_name, &
+            & "Error in oasis_put: ", rcode=kinfo)
+
+      ! advance
+      call gc_step(component, kinfo)
+      call check_err(kinfo, comp_id, comp_name, __FILE__, __LINE__)
+
+
+      ! import the temperature from ice
+      call oasis_get(import_id, date, component % top_temperature, kinfo)
+      if(kinfo<0) call oasis_abort(comp_id, comp_name, &
+            & "Error in oasis_put: ", rcode=kinfo)
+
+   enddo
+
+   ! clean up
+   call gc_del(component, kinfo)
+      call check_err(kinfo, comp_id, comp_name, __FILE__, __LINE__)
+
+   call oasis_terminate(kinfo)
+   if(kinfo<0) call oasis_abort(comp_id, comp_name, &
+      & "Error in oasis_terminate: ", rcode=kinfo)
+
+   call mpi_finalize(kinfo)
+      
+end program ocean
